aa38f7fbc223bd5d24acdedfcdc38099dac2a53d
Shubham Shinde <shubham.shinde@sshinde0523.local>
2023-05-22 17:21:35 +0530

    (PA-5030) Removing MAC OS 10.15 support from install puppet

--- install.sh
+++ install.sh
@@ -472,7 +472,6 @@ if true; then
       "10.12") platform_version="10.12";;
       "10.13") platform_version="10.13";;
       "10.14") platform_version="10.14";;
-      "10.15") platform_version="10.15";;
       "11")    platform_version="11";;
       "12")    platform_version="12";;
       *) echo "No builds for platform: $major_version"
